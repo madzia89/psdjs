eb6d161aa4089bac363b064f5b6831c2f5c3768d
madzia89 <wojcicka.ma@gmail.com>
2018-04-12 14:55:29 +0200

    merge all elements

--- js/script.js
+++ js/script.js
@@ -17,6 +17,28 @@ function removeFirst() {
     updateView()                                // aktualizujemy wygląd bez tej tablicy
 }
 
+function cloneID(index, arrValue){      //klonowanie przycisku
+    arr.splice(index, 0, arrValue)
+    updateView()
+}
+
+function mergeIt(index) {
+    console.log('Merguje pozycję z tablicy o kluczu "' + index + '"');
+    if (index > 0) {
+        arr[index - 1] += ' ' + arr[index];
+        removeElement(index);
+    }
+}
+
+function mergeThose(index) { //merge all
+    if (index > 0) {
+        var spliced = arr.splice(1, index)
+        var joined = spliced.join(' ')
+        arr[0] = arr[0] + ' ' + joined
+        updateView()
+    }
+}
+
 
 function pushElement(){//dodatnie wpisanego tekstu na koniec tablicy
     ////tutaj pobieramy watość z input o id = element-name
@@ -58,17 +80,9 @@ function drawArrayInfo(arr) {
 function drawBox(arrValue, index) {
     console.log('Rysuję BOX dla wartości "' + arrValue + '" [klucz = ' + index + ']');
     var arrayElementsDOM = document.getElementById('array-elements');
-    var html = '<div class="col-3">\n\
-<div class="box">\n\
-<div class="box-actions">\n\
-<button class="btn btn-danger btn-sm" onclick="removeElement(\'' + index+ '\')">\n\
-<span class="badge badge-secondary">'+index+'</span>\n\
-<i class="far fa-trash-alt"></i>\n\
-</button>\n\
-</div>\n\
-<h2>' + arrValue + '</h2>\n\
-</div>\n\
-</div>';
+    var html = '<div class="col-3"><div class="box"><div class="box-actions"> <button class="btn btn-danger btn-sm" onclick="removeElement(\'' + index+ '\')"><span class="badge badge-secondary">'+index+'</span><i class="far fa-trash-alt"></i></button><button class="btn btn-success btn-sm" onclick="cloneID(\'' + index+'\',\'' + arrValue+ '\')"><i class="far fa-clone"></i></button><button class="btn btn-warning btn-sm" onclick="mergeIt(\'' + index+'\',\'' + arrValue+ '\')"><i class="fas fa-compress"></i></button><button class="btn btn-secondary btn-sm" onclick="mergeThose(\'' + index+'\',\'' + arrValue+ '\')"><i class="fas fa-compress"></i></button></div><h2>' + arrValue + '</h2></div></div>';
     arrayElementsDOM.insertAdjacentHTML('beforeend', html);
 }
+
+
 
