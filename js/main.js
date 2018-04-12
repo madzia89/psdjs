082c2763a5ed06a38fe51a7fb6191abfa5897af2
madzia89 <wojcicka.ma@gmail.com>
2018-04-12 13:08:36 +0200

    git init

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// Przykładowa tablica
+var arr = ["Alfa Romeo", "Mercedes", "Skoda", "Ferrari", "Fiat", "Volkswagen", "Kamaz", "Audi", "Mercedes"];
+
+updateView();
+
+function removeElement(index) {
+    arr.splice(index, 1)                          // usuwamy wartość którą chcemy uzunąć z tablicy
+    updateView()                                // aktualizujemy wygląd bez tej tablicy
+}
+
+function removeLast() {
+    arr.pop()                         // usuwamy wartość którą chcemy uzunąć z tablicy
+    updateView()                                // aktualizujemy wygląd bez tej tablicy
+}
+function removeFirst() {
+    arr.shift()                         // usuwamy wartość którą chcemy uzunąć z tablicy
+    updateView()                                // aktualizujemy wygląd bez tej tablicy
+}
+
+
+function updateView() {
+    var arrayElementsDOM = document.getElementById('array-elements');
+    // Usunie mi istniejace boxy
+    arrayElementsDOM.innerHTML = "";
+    // Narysuje mi je ponownie
+    arr.forEach(drawBox);
+    // Aktualizuje informacje o tablicy
+    drawArrayInfo(arr);
+}
+
+// Pokazujemy informacje o tablicy
+function drawArrayInfo(arr) {
+    console.log('Rysuję informację o tablicy:');
+    console.log(arr);
+    var arrayElementsDOM = document.getElementById('array');
+    arrayElementsDOM.innerHTML = "";
+    arrayElementsDOM.insertAdjacentHTML('beforeend', '<h3><small class="text-muted pr-2">Length:</small>' + arr.length + '</h3>');
+}
+
+// Rysowanie boxu z informacja
+function drawBox(arrValue, index) {
+    console.log('Rysuję BOX dla wartości "' + arrValue + '" [klucz = ' + index + ']');
+    var arrayElementsDOM = document.getElementById('array-elements');
+    var html = '<div class="col-3">\n\
+<div class="box">\n\
+<div class="box-actions">\n\
+<button class="btn btn-danger btn-sm" onclick="removeElement(\'' + index+ '\')">\n\
+<span class="badge badge-secondary">'+index+'</span>\n\
+<i class="far fa-trash-alt"></i>\n\
+</button>\n\
+</div>\n\
+<h2>' + arrValue + '</h2>\n\
+</div>\n\
+</div>';
+    arrayElementsDOM.insertAdjacentHTML('beforeend', html);
+}
